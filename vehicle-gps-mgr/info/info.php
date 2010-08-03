c670b7037a017e280f3a49ac9643b13953307982
starecho@gmail.com <starecho@gmail.com@2fe7dcaa-6ef8-bcf5-8241-07c18bce28a8>
2010-08-03 06:57:27 +0000

    新增发布信息 查询信息 统计分析模块

---
+++ vehicle-gps-mgr/info/info.php
@@ -0,0 +1,21 @@
+<?php
+/** 
+* 人员管理
+* @copyright		秦运恒, 2010
+* @author			郭英涛
+* @create date		2010.08.02
+* @modify			修改人
+* @modify date		修改日期
+* @modify describe	修改内容
+*/
+
+$act = $GLOBALS["all"]["operate"];
+
+switch($act)
+{
+	case "main":	//填写信息内容页面
+		echo $GLOBALS['db']->display(null,$act);
+		break;
+	break;
+}
+?>
