d594d5eab9a2711904214d357735065614f0aec9
starecho@gmail.com <starecho@gmail.com@2fe7dcaa-6ef8-bcf5-8241-07c18bce28a8>
2010-08-03 02:13:14 +0000

    修改ip地址错误

--- vehicle-gps-mgr/include/config.php
+++ vehicle-gps-mgr/include/config.php
@@ -1,7 +1,7 @@
 <?php
 //		数据库配置
 $db_config = array(
-	'HOST'			=> '220.194.47,152',
+	'HOST'			=> '220.194.47.152',
 	'USERNAME'			=> 'qinyh',
 	'PASSWORD'			=> '608',
 	'DB'		=> 'vehicle_gps_mgr');
