f11c77e56b7f26c6bea151caf74de0f79d87e14d
suyuanyuan.87@gmail.com <suyuanyuan.87@gmail.com@2fe7dcaa-6ef8-bcf5-8241-07c18bce28a8>
2010-08-04 13:14:47 +0000

    列表样式 居中

--- vehicle-gps-mgr/js/vehicle_group/vehicle_group.js
+++ vehicle-gps-mgr/js/vehicle_group/vehicle_group.js
@@ -4,10 +4,10 @@ jQuery("#navgrid_vehicle_group").jqGrid({
 		datatype: "json",
    	colNames:['ID','组名', '公司','描述'],
    	colModel:[
-   		{name:'id',index:'id', width:55,editable:false,hidden:true,editoptions:{readonly:true,size:10}},
-   		{name:'name',index:'name', width:80,editable:true,editrules:{required:true},editoptions:{size:10}},
-   		{name:'company_id',index:'company_id',width:40,editable:true,hidden:true,edittype:"select",editoptions:{dataUrl:'index.php?a=1023&p=company_id'}},
-   		{name:'description',index:'description',width:60,editable:true,editoptions:{size:10}}
+   		{name:'id',index:'id', align:"center",width:55,editable:false,hidden:true,editoptions:{readonly:true,size:10}},
+   		{name:'name',index:'name',align:"center", width:80,editable:true,editrules:{required:true},editoptions:{size:10}},
+   		{name:'company_id',index:'company_id',align:"center",width:40,editable:true,hidden:true,edittype:"select",editoptions:{dataUrl:'index.php?a=1023&p=company_id'}},
+   		{name:'description',index:'description',align:"center",width:60,editable:true,editoptions:{size:10}}
 //   		{name:'create_id',index:'driver_id', width:60, align:"right",editable:true,editoptions:{size:10}},
 //   		{name:'create_time',index:'type_id', width:60, align:"right",editable:true,editoptions:{size:10}},
 //   		{name:'update_id',index:'cur_longitude', width:50, align:"right",editable:true,editoptions:{size:10}},
--- vehicle-gps-mgr/js/vehicle_type/vehicle_type.js
+++ vehicle-gps-mgr/js/vehicle_type/vehicle_type.js
@@ -3,12 +3,12 @@ jQuery("#navgrid_vehicle_type").jqGrid({
 		datatype: "json",
    	colNames:['ID','公司ID', '类型名称','耗油指标','载重','描述'],
    	colModel:[
-   		{name:'id',index:'id', width:55,editable:false,hidden:true,editoptions:{size:10}},
-   		{name:'company_id',index:'company_id',hidden:true,editable:true,editoptions:{size:10}},
-   		{name:'name',index:'name', width:80,editable:true,editrules:{required:true},editoptions:{size:10}},
-   		{name:'fuel_consumption',index:'fuel_consumption',width:40,editable:true,editoptions:{size:10}},
-   		{name:'load_capacity',index:'load_capacity',width:40,editable:true,editoptions:{size:10}},
-   		{name:'description',index:'description',width:60,editable:true,editoptions:{size:10}}
+   		{name:'id',index:'id',align:"center", width:55,editable:false,hidden:true,editoptions:{size:10}},
+   		{name:'company_id',index:'company_id',align:"center",hidden:true,editable:true,editoptions:{size:10}},
+   		{name:'name',index:'name', align:"center",width:80,editable:true,editrules:{required:true},editoptions:{size:10}},
+   		{name:'fuel_consumption',index:'fuel_consumption',align:"center",width:40,editable:true,editoptions:{size:10}},
+   		{name:'load_capacity',index:'load_capacity',align:"center",width:40,editable:true,editoptions:{size:10}},
+   		{name:'description',index:'description',align:"center",width:60,editable:true,editoptions:{size:10}}
 //   		{name:'create_id',index:'driver_id', width:60, align:"right",editable:true,editoptions:{size:10}},
 //   		{name:'create_time',index:'type_id', width:60, align:"right",editable:true,editoptions:{size:10}},
 //   		{name:'update_id',index:'cur_longitude', width:50, align:"right",editable:true,editoptions:{size:10}},
