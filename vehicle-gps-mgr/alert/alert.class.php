ab20c9a29b70b28d1504c3e986ed1b1c88fe2ed2
duanguishan@gmail.com <duanguishan@gmail.com@2fe7dcaa-6ef8-bcf5-8241-07c18bce28a8>
2010-08-05 06:47:53 +0000

    查询所有车辆数据(和车辆组联动),算出车辆组下对应的车辆数

--- vehicle-gps-mgr/alert/alert.class.php
+++ vehicle-gps-mgr/alert/alert.class.php
@@ -22,19 +22,18 @@ class Alert extends BASE {
 
 	private $alert_id = false; //人员ID
 	
-  /**
-	*		构造函数
-	*		@param $alert_id 
-	*		@return no
-	*/
-	function Alert($alert_id=false)
-	{
-		if($alert_id && !empty($alert_id))
-		{
+
+	/**
+	 * 构造函数
+	 * @param $alert_id 
+	 * @return no
+	 */
+	function Alert($alert_id = false) {
+		if ($alert_id && ! empty ( $alert_id )) {
 			$this->alert_id = $alert_id;
 		}
 	}
-
+	
 	/**
 	 * 查询所有记录
 	 * @param $wh
@@ -55,8 +54,8 @@ class Alert extends BASE {
 	 * @return 总记录数
 	 * 
 	 */
-	function get_all_count() {
-		$this->sql = "select count(*) from " . $this->mysel_table_name;
+	function get_all_count($condition) {
+		$this->sql = "select count(*) from " . $this->mysel_table_name.$condition;
 		$count = $GLOBALS ["db"]->query_once ( $this->sql );
 		return $count [0];
 	}
@@ -85,21 +84,55 @@ class Alert extends BASE {
 	 * @param $id
 	 * @param $advice
 	 */
-	function edit_alert_advice($alert){
-		if(!$alert)
-		{
+	function edit_alert_advice($alert) {
+		if (! $alert) {
 			$this->message = "error,object must be not empty!";
 			return false;
 		}
 		//添加主键ID
-		$alert['id'] = $this->alert_id;
-		if(!$GLOBALS['db']->update_row($this->mysel_table_name,$alert,"id"))
-		{
+		$alert ['id'] = $this->alert_id;
+		if (! $GLOBALS ['db']->update_row ( $this->mysel_table_name, $alert, "id" )) {
 			$this->message = "error,edit data failed!";
 			return false;
 		}
 		return true;
 	}
+
+	/**
+	 * 查询所有车辆组数据
+	 * @return 查询记录
+	 */
+	function get_vehicle_group() {
+		$this->sql = "select id,name from vehicle_group";
+		return $this->data = $GLOBALS ["db"]->query ( $this->sql );
+	}
+	/**
+	 * 查询所有车辆数据
+	 * @return 查询记录
+	 */
+	function get_vehicle() {
+		$this->sql = "select id,number_plate from vehicle_manage";
+		return $this->data = $GLOBALS ["db"]->query ( $this->sql );
+	}
+	/**
+	 * 查询所有车辆数据(和车辆组联动)
+	 * @param unknown_type $vehicle_group_id
+	 */
+	
+	function get_linkage_vehicle($vehicle_group_id=""){
+		$this->sql ="select vm.id,vm.number_plate from vehicle_manage as vm inner join  vehicle_group as vg".
+                    " on vg.id=vm.vehicle_group_id and vm.vehicle_group_id=".$vehicle_group_id;
+		return $this->data = $GLOBALS ["db"]->query ( $this->sql );
+	}
+	/**
+	 * 算出车辆组下对应的车辆数
+	 */
+	function get_count_vehicle($vehicle_group_id=""){
+		$this->sql ="select count(number_plate) from vehicle_manage as vm inner join  vehicle_group as vg
+         on vg.id=vm.vehicle_group_id and vm.vehicle_group_id=".$vehicle_group_id;
+		return $this->data = $GLOBALS ["db"]->query ( $this->sql );
+	}
+	
 	/**
 	 * 获得24小时内最新未处理告警记录
 	 */
