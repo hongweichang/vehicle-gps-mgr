4d93dca54d458195cc6df7e98d250e757005eedc
duanguishan@gmail.com <duanguishan@gmail.com@2fe7dcaa-6ef8-bcf5-8241-07c18bce28a8>
2010-08-05 06:51:37 +0000

    修改告警模块 增加联动 查询

--- vehicle-gps-mgr/js/alert/advice.js
+++ vehicle-gps-mgr/js/alert/advice.js
@@ -1,65 +1,93 @@
 
-function assignToAdvice(){
-	var defaul=$('#defaultAdvice option:selected').text();
-	$("#advice").text(defaul);	
-}
-function clearText(){
-	$("#advice").text("");	
-}
+			
+					function assignToAdvice() {
+						var defaul = $('#defaultAdvice option:selected').text();
+						$("#advice").text(defaul);
+					}
+					function clearText() {
+						$("#advice").text("");
+					}
 
-function adviceDialog(id,length,str){
+					function adviceDialog(id, length, str) {
 
-    jQuery("#dialog").dialog({
-			bgiframe: true,
-			autoOpen: true, 
-			height: 375, 
-			width: 280,
-			modal: true,
-			title: '填写告警处理意见',
-			buttons: {
-			    '取消': function() {
-					jQuery(this).dialog('close');
-			  },'提交': function() { 
-				      var val = $("#advice").val(); 
-				      if(val==""||val==null){
-					      alert("请填写告警处理意见");
-					      return false;
-				      }else{
-				    	  jQuery.ajax({
-								url : "index.php?a=903",								
-								type : "post", 
-								data:"advice="+val+"&id="+id, 
-								success : function(data) { 
-								           if(data == "success"){							
-								              location.href='index.php?a=901'; //刷新当前页面.
-								           }else  if(data == "fail"){									           
-								              alert("添加处理意见失败！"); 
-								           }
-								},error : function(XMLHttpRequest,textStatus,errorThrown){
-							                  alert("Errors: "+errorThrown);
-							                  jQuery(this).dialog('close');
-							    }
-							}); 
-				      }       	    
-				}
-            },open:function(event,ui){
-			     var selHtml="";
-            	 var arr  = str.split("|");
+						jQuery("#dialog")
+								.dialog(
+										{
+											bgiframe : true,
+											autoOpen : true,
+											height : 375,
+											width : 280,
+											modal : true,
+											title : '填写告警处理意见',
+											buttons : {
+												'取消' : function() {
+													jQuery(this)
+															.dialog('close');
+												},
+												'提交' : function() {
+													var val = $("#advice")
+															.val();
+													if (val == ""
+															|| val == null) {
+														alert("请填写告警处理意见");
+														return false;
+													} else {
+														jQuery
+																.ajax( {
+																	url : "index.php?a=903",
+																	type : "post",
+																	data : "advice="
+																			+ val
+																			+ "&id="
+																			+ id,
+																	success : function(
+																			data) {
+																		if (data == "success") {
+																			location.href = 'index.php?a=901'; // 刷新当前页面.
+																		} else if (data == "fail") {
+																			alert("添加处理意见失败！");
+																		}
+																	},
+																	error : function(
+																			XMLHttpRequest,
+																			textStatus,
+																			errorThrown) {
+																		alert("Errors: "
+																				+ errorThrown);
+																		jQuery(
+																				this)
+																				.dialog(
+																						'close');
+																	}
+																});
+													}
+												}
+											},
+											open : function(event, ui) {
+												var selHtml = "";
+												var arr = str.split("|");
 
-                 for(var i=0;i<length;i++){
-                     var a=arr[i].split(",");
-                     selHtml=selHtml+"<option value="+a[0]+">"+a[1]+"</option>";             
-                 }
-            	
-            	              
-            	jQuery('body').css('overflow','hidden');
-		        jQuery("#canAdviceSelectDiv").html("<font style='font-weight:bold'>默认告警处理意见:</font>"+
-				       "<div margin-top='3'><select id='defaultAdvice' style='width:100px' onchange='assignToAdvice()'>"+selHtml+"</select></div><br>"+
-				       "<font style='font-weight:bold'>您的告警处理意见:</font><br>"+
-				       "<textarea style='OVERFLOW:hidden;"+
-				       "border-style:groove; height:160px; width:200px;' name='advice' id='advice' onFocus='clearText();' >"+
-				        "--告警处理意见--</textarea> "); 
-            }
-	 });
+												for ( var i = 0; i < length; i++) {
+													var a = arr[i].split(",");
+													selHtml = selHtml
+															+ "<option value="
+															+ a[0] + ">" + a[1]
+															+ "</option>";
+												}
 
-}
+												jQuery('body').css('overflow',
+														'hidden');
+												jQuery("#canAdviceSelectDiv")
+														.html(
+																"<font style='font-weight:bold'>默认告警处理意见:</font>"
+																		+ "<div margin-top='3'><select id='defaultAdvice' style='width:100px' onchange='assignToAdvice()'>"
+																		+ selHtml
+																		+ "</select></div><br>"
+																		+ "<font style='font-weight:bold'>您的告警处理意见:</font><br>"
+																		+ "<textarea style='OVERFLOW:hidden;"
+																		+ "border-style:groove; height:160px; width:200px;' name='advice' id='advice' onFocus='clearText();' >"
+																		+ "--告警处理意见--</textarea> ");
+											}
+										});
+
+					}
--- vehicle-gps-mgr/js/alert/alert.js
+++ vehicle-gps-mgr/js/alert/alert.js
@@ -1,70 +1,70 @@
 
+$(document).ready(function() {
+	jQuery("#navgrid1").jqGrid( {
+		url : 'index.php?a=902',
+		datatype : "json",
+		colNames : [ 'id', '告警时间', '告警类型', '车牌号码', '处理人姓名', '描述', '处理状态' ],
+		colModel : [ {
+			name : 'id',
+			index : 'id',
+			width : 55,
+			editable : false,
+			hidden : true
 
-jQuery("#navgrid1").jqGrid( {
-	url : 'index.php?a=902',
-	datatype : "json",
-	colNames : [ 'id', '告警时间', '告警类型', '车牌号码', '处理人姓名',  '描述','处理状态' ],
-	colModel : [ {
-		name : 'id',
-		index : 'id',
-		width : 55,
-		editable : false,
-		hidden : true
-		
-	}, {
-		name : 'alert_time',
-		index : 'alert_time',
-		width : 50,
-		align : "center",
-		editable : false
-	}, {
-		name : 'alert_type',
-		index : 'alert_type',
-		width : 50,
-		align : "center",
-		editable : false
-	}, {
-		name : 'vehicle_id',
-		index : 'vehicle_id',
-		width : 50,
-		align : "center",
-		editable : false
-	}, {
-		name : 'dispose_id',
-		index : 'dispose_id',
-		width : 50,
-		align : "center",
-		editable : false
-	},{
-		name : 'description',
-		index : 'description',
-		width : 50,
-		align : "center",
-		editable : false
-	},{
-		name : 'dealStatus',
-	
-		width : 50,
-		align : "center",
-		editable : false
-	}
-	 ],
-	rowNum : 10,// 初始化每页10条数据
-	rowList : [ 10, 20, 30 ],// 设置每页多少条数据
-	mtype : "GET",
-	rownumbers : true,
-	rownumWidth : 60,
-	gridview : true,
-	pager : '#pagernav1',
-	viewrecords : true,
-	sortorder : "asc",
-	caption : "告警信息",
-	height : 230,
-	width : 800
-});
-jQuery("#navgrid1").jqGrid('navGrid','#pagernav1', {
-	del : false,
-	add : false,
-	edit : false,
-	alerttext : "请选择需要操作的数据行!"
-});
+		}, {
+			name : 'alert_time',
+			index : 'alert_time',
+			width : 50,
+			align : "center",
+			editable : false
+		}, {
+			name : 'alert_type',
+			index : 'alert_type',
+			width : 50,
+			align : "center",
+			editable : false
+		}, {
+			name : 'vehicle_id',
+			index : 'vehicle_id',
+			width : 50,
+			align : "center",
+			editable : false
+		}, {
+			name : 'dispose_id',
+			index : 'dispose_id',
+			width : 50,
+			align : "center",
+			editable : false
+		}, {
+			name : 'description',
+			index : 'description',
+			width : 50,
+			align : "center",
+			editable : false
+		}, {
+			name : 'dealStatus',
+
+			width : 50,
+			align : "center",
+			editable : false
+		} ],
+		rowNum : 10,// 初始化每页10条数据
+		rowList : [ 10, 20, 30 ],// 设置每页多少条数据
+		mtype : "GET",
+		rownumbers : true,
+		rownumWidth : 60,
+		gridview : true,
+		pager : '#pagernav1',
+		viewrecords : true,
+		sortorder : "asc",
+		caption : "告警信息",
+		height : 230,
+		width : 800
+	});
+	jQuery("#navgrid1").jqGrid('navGrid', '#pagernav1', {
+		del : false,
+		add : false,
+		edit : false,
+		alerttext : "请选择需要操作的数据行!"
+	});
+})
