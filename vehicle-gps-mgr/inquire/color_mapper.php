f94cb8885e9303084ac119e5db6d728bd371466f
starecho@gmail.com <starecho@gmail.com@2fe7dcaa-6ef8-bcf5-8241-07c18bce28a8>
2010-08-09 21:21:23 +0000

    新增颜色速度映射处理类

---
+++ vehicle-gps-mgr/inquire/color_mapper.php
@@ -0,0 +1,58 @@
+<?php
+class Color_mapper
+{
+	private $speed_color_list = array();
+	
+	/**
+	 * 根据行车速度得到相应的颜色值
+	 * @param unknown_type $speed
+	 */
+	function get_color($speed, $company_id)
+	{
+		//查看Session中是否有关于速度颜色映射的信息
+		//如果有，则直接利用
+		if($_SESSION["speed_color"])
+		{
+			$speed_color_list = $_SESSION["speed_color"];
+			return $this->get_color_by_speed($speed, $speed_color_list);
+		}
+		else //如果没有，则从数据库中查询出来，然后设置到Session中
+		{
+			//查询数据库
+			$sql = sprintf("select min, max, color from speed_color where company_id = %d", $company_id);
+			$data = $GLOBALS["db"]->query($sql);
+			
+			foreach($data as $value)
+			{
+				$speedStr = $value['min']."-".$value['max'];
+				$speed_color_list[$speedStr] = $value['color'];		
+			}
+			
+			$_SESSION["speed_color"] = $speed_color_list;
+			return $this->get_color_by_speed($speed, $speed_color_list);
+		} 
+	}
+	
+	/**
+	 * 根据速度颜色映射表得到所需的颜色值
+	 * @param $speed
+	 * @param $speed_color_list
+	 */
+	private function get_color_by_speed($speed, $speed_color_list)
+	{
+		foreach($speed_color_list as $key=>$value)
+		{
+			$speedPair = explode('-',$key);
+			$speed_min = $speedPair[0];
+			$speed_max = $speedPair[1];
+			
+			if(($speed >= $speed_min) && ($speed < $speed_max))
+			{
+				return $value;
+			}
+		}
+		
+		return "";
+	}
+}
+?>
