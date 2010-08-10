141dcb14d41e97fdc3b59bce9193b4a85e4d79a9
shi_enxiu@163.com <shi_enxiu@163.com@2fe7dcaa-6ef8-bcf5-8241-07c18bce28a8>
2010-08-10 12:12:33 +0000

    修改查询告警的sql文，加了公司id限制

--- vehicle-gps-mgr/alert/alert.class.php
+++ vehicle-gps-mgr/alert/alert.class.php
@@ -137,10 +137,12 @@ class Alert extends BASE {
 	 * 获得最新未处理告警记录
 	 */
     function get_newest_alert(){
+    	$company_id = get_session("company_id");
     	//格式化sql语句
-    	$sql ="select a.id,a.alert_time, v.number_plate, a.description from alert_info a inner join vehicle_manage v where  v.id=a.vehicle_id  and (a.dispose_opinion is null or a.dispose_opinion=\"\")  order by a.alert_time desc limit 0,1" ;
+    	$sql ="select a.id,a.alert_time, v.number_plate, a.description from alert_info a left join vehicle_manage v on v.id=a.vehicle_id left join company c on a.vehicle_id=c.id
+    	where  (a.dispose_opinion is null or a.dispose_opinion=\"\") and c.id=%s  order by a.alert_time desc limit 0,1" ;
     	
-    	$this->sql = sprintf($sql);
+    	$this->sql = sprintf($sql,$company_id);
     	
     	$record = $GLOBALS ["db"]->query_once ( $this->sql );
     	return $record;
