4c22af59d101ab5435c96e6eae26ed9462fa7f80
zhaojiangwei6464@163.com <zhaojiangwei6464@163.com@2fe7dcaa-6ef8-bcf5-8241-07c18bce28a8>
2010-08-10 16:31:57 +0000

    更新车辆状态显示js

--- vehicle-gps-mgr/js/vehicle/vehicle_status.js
+++ vehicle-gps-mgr/js/vehicle/vehicle_status.js
@@ -1,12 +1,13 @@
 jQuery("#vehicle_status_list").jqGrid({
 	url:'index.php?a=502',
 	  datatype: "json",
-   	colNames:['ID','车牌号', 'GPS状态', '定位时间','实速','限速','驾驶员','告警状态','轨迹','统计信息','信息发布','定位','当前位置'],
+   	colNames:['ID','车牌号', 'GPS状态', '定位时间','当前位置','实速','限速','驾驶员','告警状态','轨迹','统计信息','信息发布','定位'],
    	colModel:[
    		{name:'id',index:'id', width:55,editable:false,hidden:true,editoptions:{readonly:true,size:10}},
    		{name:'number_plate',index:'number_plate', width:80},
    		{name:'gps_status',index:'gps_status', width:70},
-   		{name:'location_time',index:'location_time', width:70, align:"left"},   		
+   		{name:'location_time',index:'location_time', width:70, align:"left"},   
+   		{name:'cur_location',index:'cur_location', width:600, align:"left"},
    		{name:'real_speed',index:'real_speed', width:60, align:"left"},
    		{name:'speed_limit',index:'speed_limit', width:50, align:"left"},
    		{name:'driver',index:'driver', editable:true,width:70, align:"left"},
@@ -14,8 +15,8 @@ jQuery("#vehicle_status_list").jqGrid({
    		{name:'trace',index:'trace', width:60, align:"left"},
    		{name:'statistic',index:'statistic', width:70, align:"left"},
    		{name:'info',index:'info', width:70, align:"left"},
-   		{name:'location',index:'location', width:60,align:"left"},
-   		{name:'cur_location',index:'cur_location', width:600, align:"left"}
+   		{name:'location',index:'location', width:60,align:"left"}
+   		
    	],
    	rowNum:10,
    	rowList:[10,20,30],
@@ -30,7 +31,7 @@ jQuery("#vehicle_status_list").jqGrid({
 	loadComplete:function(){
 	var ids = $("#vehicle_status_list").jqGrid('getDataIDs');
 	for(var i = 1;i<=ids.length;i++){
-		var value = $("#vehicle_status_list").jqGrid('getCell',i,7);
+		var value = $("#vehicle_status_list").jqGrid('getCell',i,8);
 		if(value=="超速"){
 			$("#vehicle_status_list").jqGrid('setRowData',i,"",{background:'#ff0000'});
 		}
