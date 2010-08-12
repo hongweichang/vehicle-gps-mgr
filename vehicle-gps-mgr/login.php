b14b5de677c9734d185f3a6d8b45f47dee99441b
shi_enxiu@163.com <shi_enxiu@163.com@2fe7dcaa-6ef8-bcf5-8241-07c18bce28a8>
2010-08-12 06:33:30 +0000

    引入记住客户端内容js

--- vehicle-gps-mgr/login.php
+++ vehicle-gps-mgr/login.php
@@ -1,23 +1,25 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>GPS智能车辆监控调度系统-登录</title>
-<link href="css/login.css" rel="stylesheet" />
-<script type="text/javascript">
-	function mouseOver(){
-		document.getElementById("loginCar").src='images/car.png';
-		document.getElementById("loginCar").style.cursor='hand';
-	}
-	function mouseOut(){
-		document.getElementById("loginCar").src='images/ycar.png';
-	}
-
-	function login(){
-			document.getElementById("login_from").submit();
-
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>GPS智能车辆监控调度系统-登录</title>
+	<link href="css/login.css" rel="stylesheet" />
+	
+	<script language="javascript" src="../js/login/cookie.js"></script>
+	<script language="javascript" src="../js/login/common.js"></script>
+	<script type="text/javascript">
+		function mouseOver(){
+			document.getElementById("loginCar").src='images/car.png';
+			document.getElementById("loginCar").style.cursor='hand';
 		}
-</script>
+		function mouseOut(){
+			document.getElementById("loginCar").src='images/ycar.png';
+		}
+	
+		function login(){
+				document.getElementById("login_from").submit();
+		}
+	</script>
 </head>
 
 <body>
@@ -43,22 +45,28 @@
 		   		<div style="background-image:url(images/spacer.gif);width:1px; height:180px; float:left;"></div>
 	       </div>
 			<div class="conetnt_div">
-				<div class="companyID">
-					<div class="float_text">公司ID：</div>
-					<div class="float_left">
-						<input type="text" name="companyloginid" class="text_password_input" />
+				<div class="companyID" style="border:1px red solid;">
+					<div class="float_text" style="border:1px red solid;">公司ID：</div>
+					<div class="float_left" style="border:1px red solid;">
+						<input type="text" name="companyloginid" id="companyId" class="text_password_input" />
+						<input id="saveCompanyId" type="checkbox" checked="checked" value="" />
+						<span style="font-size:12px; color:blue;">记住</span>
 					</div>
 				</div>
-				<div class="input_div">
-					<div class="float_text">用户ID：</div>
-					<div class="float_left">
-						<input type="text" name="username"  class="text_password_input"/>
+				<div class="input_div" style="border:1px red solid;">
+					<div class="float_text" style="border:1px red solid;">用户ID：</div>
+					<div class="float_left" style="border:1px red solid;">
+						<input type="text" name="username" id="userName"  class="text_password_input"/>
+						<input id="saveUserName" type="checkbox" checked="checked" value="" />
+						<span style="font-size:12px; color:blue;">记住</span>
 					</div>
 				</div>
-				<div class="input_div">
-					<div class="float_text">密</div><div class="text" >码：</div>
-					<div class="float_left">
-						<input type="password" name="password" class="text_password_input" />
+				<div class="input_div" style="border:1px red solid;">
+					<div class="float_text" style="border:1px red solid;">密</div><div class="text" style="border:1px red solid;">码：</div>
+					<div class="float_left" style="border:1px red solid;">
+						<input type="password" name="password" id="password" class="text_password_input" />
+						<input id="savePassWord" type="checkbox" value="" />
+						<span style="font-size:12px; color:blue;">记住</span>
 					</div>
 				</div>
 				<div class="submit_div">
@@ -70,3 +78,4 @@
 </form>
 </body>
 </html>
+		
