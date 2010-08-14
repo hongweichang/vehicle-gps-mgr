dbb7c3b47adc2e55438107b03f706603d37593ab
duanguishan@gmail.com <duanguishan@gmail.com@2fe7dcaa-6ef8-bcf5-8241-07c18bce28a8>
2010-08-14 13:09:34 +0000

    解决冲突

--- vehicle-gps-mgr/statistic/statistic.php
+++ vehicle-gps-mgr/statistic/statistic.php
@@ -21,6 +21,8 @@ $end_data_str=$_REQUEST["end_data"];//得到查询结束时间
 $begin_data=str_replace("/","-",$begin_data_str);
 $end_data=str_replace("/","-",$end_data_str);
 
+$driver_id=$_REQUEST["driver_id"];//得到驾驶员id 
+
 switch ($act) {
 	case "main" : //填写信息内容页面
 		echo $GLOBALS ['db']->display ( null, $act );
@@ -135,7 +137,9 @@ switch ($act) {
 
 			$response->rows [$key] ['driver_id'] = $value ['driver_id'];
 			
+
 			$response->rows [$key] ['cell'] = array ($value ['driver_id'], $value ['name'], $value ['distance'], $value ['drive_time'], $value ['stop_time'], $value ['min_time'], $value ['max_time'], "<a href='#' onclick='show_driver(" . $value ['driver_id'] . ")' style='text-decoration:none;color:#0099FF';font-size:12px;>详细内容</a>" );
+
 		}
 		echo json_encode ( $response ); //打印json格式的数据
 		break;
@@ -169,10 +173,60 @@ switch ($act) {
 		foreach ( $dataList as $key => $value ) { //从xml文件中映射相应的数据库字段值
 			
 			$response->rows [$key] ['vehicle_id'] = $value ['vehicle_id'];	
+
 			$response->rows [$key] ['cell'] = array ($value ['vehicle_id'], $value ['number_plate'], $value ['distance'], $value ['drive_time'], $value ['stop_time'], $value ['min_time'], $value ['max_time'], "<a href='#' onclick='show_vehicle(" . $value ['vehicle_id'] . ")' style='text-decoration:none;color:#0099FF';font-size:12px;>详细内容</a>" );
+
 		}
 		echo json_encode ( $response ); //打印json格式的数据	
 		break;
+		
+  
+		
+		
+//**************************************************************	
+   case "drive_detail_list" ://显示驾驶员开车列表
+   	       echo $GLOBALS ['db']->display ( null, $act );
+	    break;
+   	    
+  case  "drive_detail_data" ://显示驾驶员开车列表数据
+   	if (! $sidx)
+			$sidx = 1;
+		$wh = "where 1=1 "; //查询条件
+		$limit_length = 8; //设置处理意见字符串最多显示8个字符
+		$driver = new Statistic ();
+		
+		$count = $driver->drive_detail_count($driver_id);
+		
+		if ($count > 0) {
+			$total_pages = ceil ( $count / $limit );
+		} else {
+			$total_pages = 0;
+		}
+		if ($page > $total_pages)
+			$page = $total_pages;
+		$start = $limit * $page - $limit;
+		if ($start < 0)
+			$start = 0;
+		
+		$dataList = $driver->drive_detail_data($driver_id, $wh, $sidx, $sord, $start, $limit );
+		
+		$response->page = $page; //分别赋值当前页,总页数，总数据条数
+		$response->total = $total_pages;
+		$response->records = $count;
+		
+		foreach ( $dataList as $key => $value ) { //从xml文件中映射相应的数据库字段值
+			
+			$response->rows [$key] ['id'] = $value ['id'];	
+			$response->rows [$key] ['cell'] = array ($value ['id'], $value ['beginning_time'], $value ['end_time'], $value ['drive_distance']);
+		}
+		echo json_encode ( $response ); //打印json格式的数据	
+   	    break;
+   	     
+    case "stop_detail_list"://显示驾驶员停车列表
+		   echo $GLOBALS ['db']->display ( null, $act );
+		    break;
+	case "stop_detail_data" ://显示驾驶员停车列表数据
+		    break;
 
 }
 ?>
