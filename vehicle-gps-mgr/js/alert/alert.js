61e23b853ce71e74f0896270110de5e8662196a4
starecho@gmail.com <starecho@gmail.com@2fe7dcaa-6ef8-bcf5-8241-07c18bce28a8>
2010-08-14 18:43:22 +0000

    恢复告警意见关闭后的告警列表的刷新动作。

--- vehicle-gps-mgr/js/alert/alert.js
+++ vehicle-gps-mgr/js/alert/alert.js
@@ -1,4 +1,6 @@
-
+	$( "#opinion" ).dialog({
+		   close: function(event, ui) { reloadGrid(); }
+	});
 	
 	jQuery("#navgrid1").jqGrid( {
 		url : "index.php?a=902&group_id="+$("#vehicle_group").val()+"&vehicle_id="+$("#vehicle_sel").val()+"&deal="+$("#data_condition").attr("checked"),
