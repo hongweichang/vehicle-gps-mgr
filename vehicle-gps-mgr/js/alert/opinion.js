6b42020d144a17cd687d21d2d509d0b7903c3121
starecho@gmail.com <starecho@gmail.com@2fe7dcaa-6ef8-bcf5-8241-07c18bce28a8>
2010-08-14 19:33:11 +0000

    修改页面尺寸 增加加载提示

--- vehicle-gps-mgr/js/alert/opinion.js
+++ vehicle-gps-mgr/js/alert/opinion.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
-	$("#opinion").dialog({ title:"处理意见", autoOpen: false, modal: true, overlay: { color: '#006' }, width: 450, height: 450 });
+	$("#opinion").dialog({ title:"处理意见", autoOpen: false, modal: true, overlay: { color: '#006' }, width: 240, height: 280 });
 });
 
 function showOpinion(id){
 	$("#opinion").html("");
+	$("#opinion").dialog("open");
+	$("#opinion").mask("处理中...");
 	$.get("index.php?a=903&id="+id, function(data) {
 		$("#opinion").html(data);
+		$("#opinion").unmank();
     });
-	$("#opinion").dialog("open");
 }
