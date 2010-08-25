f3326226efd13b307ea49089c9852328051c91fc
zhaojiangwei6464@163.com <zhaojiangwei6464@163.com@2fe7dcaa-6ef8-bcf5-8241-07c18bce28a8>
2010-08-25 02:47:47 +0000

    修改了登录时的记住功能，使它能记住东西了。

--- vehicle-gps-mgr/js/login/common.js
+++ vehicle-gps-mgr/js/login/common.js
@@ -2,30 +2,30 @@ function $(objStr){return document.getElementById(objStr);}
 window.onload = function(){
     //分析cookie值，显示上次的登陆信息
 	var companyIdValue = getCookieValue("companyId");
-     $("companyId").value = companyIdValue;
+     $("#companyId").val(companyIdValue);
 	 
     var userNameValue = getCookieValue("userName");
-     $("userName").value = userNameValue;
+     $("#userName").val( userNameValue);
 	 
     var passwordValue = getCookieValue("password");
-     $("password").value = passwordValue;
+     $("#password").val(passwordValue);
 	 
      var checkValue = getCookieValue("saveall");
-     $("saveall").checked = checkValue;
+     $("#saveall").attr("checked" , checkValue);
      
     //写入点击事件
-     $("loginCar").onclick = function()
+     $("#loginCar").click( function()
      {
-        if( $("saveall").checked){  
-			setCookie("companyId",$("companyId").value,24*30,"/");  
-		 	setCookie("userName",$("userName").value,24*30,"/"); 
-		 	setCookie("password",$("password").value,24*30,"/");
-		 	setCookie("saveall",$("saveall").checked,24*30,"/");
+        if( $("#saveall").attr("checked")==true){  
+			setCookie("companyId",$("#companyId").val(),24*30,"/");  
+		 	setCookie("userName",$("#userName").val(),24*30,"/"); 
+		 	setCookie("password",$("#password").val(),24*30,"/");
+		 	setCookie("saveall",$("#saveall").attr("checked"),24*30,"/");
 		 } else{
 			 deleteCookie("companyId","/");  
 			 deleteCookie("userName","/"); 
 			 deleteCookie("password","/");
 			 deleteCookie("saveall","/");
 		 }
-	}  
+	});  
 }
