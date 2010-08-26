707da59fb1d00d85a23f8ac2b5cf380d4db70d33
duanguishan@gmail.com <duanguishan@gmail.com@2fe7dcaa-6ef8-bcf5-8241-07c18bce28a8>
2010-08-26 03:03:05 +0000

    增加注视去掉多余方法

--- vehicle-gps-mgr/statistic/statistic.php
+++ vehicle-gps-mgr/statistic/statistic.php
@@ -30,69 +30,26 @@ switch ($act) {
 		echo $GLOBALS ['db']->display ( null, $act );
 		break;
 	    
-	case "vehicle_list" :
+	case "vehicle_list" :  //生成车辆列表页面
 		$param["VEHICLE_ID"]=$_REQUEST["vehicle_id"];//得到单一车辆信息id
 		if(empty($param["VEHICLE_ID"])){
-			$param["VEHICLE_ID"]=-1;
+			$param["VEHICLE_ID"]=-1;//如果没有取得要查询的车辆 则让隐藏于为-1传到车辆列表页面
 		}
 		echo $GLOBALS ['db']->display ($param,$act);
 		break;
 	
-	case "vehicle_list_data" :
-		$vehicle_id_value=$_REQUEST["vehicle_id_value"];//得到单一车辆信息id
-		
-		if (! $sidx)
-			$sidx = 1;
-		$wh = "where 1=1 "; //查询条件
-		$limit_length = 8; //设置处理意见字符串最多显示8个字符
-		$driver = new Statistic ();
-		
-		if($vehicle_id_value==-1){
-		   $count = $driver->get_vehicle_count ($company_id);
-		}else{
-		   $count = $driver->get_vehicle_count ($company_id,$vehicle_id_value);
-		}
-		
-		if ($count > 0) {
-			$total_pages = ceil ( $count / $limit );
-		} else {
-			$total_pages = 0;
-		}
-		if ($page > $total_pages)
-			$page = $total_pages;
-		$start = $limit * $page - $limit;
-		if ($start < 0)
-			$start = 0;
-			
-		if($vehicle_id_value==-1){
-		    $dataList = $driver->get_all_vehicle ( $wh, $sidx, $sord, $start, $limit, $company_id);
-		}else{
-			$dataList = $driver->get_all_vehicle ( $wh, $sidx, $sord, $start, $limit, $company_id,$vehicle_id_value);
-		}
-		
-		$response->page = $page; //分别赋值当前页,总页数，总数据条数
-		$response->total = $total_pages;
-		$response->records = $count;
-		
-		foreach ( $dataList as $key => $value ) { //从xml文件中映射相应的数据库字段值
-			$response->rows [$key] ['vehicle_id'] = $value ['vehicle_id'];
-			$response->rows [$key] ['cell'] = array ($value ['vehicle_id'], $value ['number_plate'], $value ['distance'], $value ['drive_time'], $value ['stop_time'], $value ['min_time'], $value ['max_time'], "<a href='#' onclick='show_vehicle(" . $value ['vehicle_id'] . ")' style='text-decoration:none;color:#0099FF';font-size:12px;>详细内容</a>" );
-		}
-		echo json_encode ( $response ); //打印json格式的数据		
-		break;
-	
-	case "driver_list" :
+	case "driver_list" :  //驾驶员列表页面
 		echo $GLOBALS ['db']->display ( null, $act );
 		break;
 	
-	case "driver_list_data" :
+	case "driver_list_data" :  //给驾驶员列表页面填充数据
 		if (! $sidx)
 			$sidx = 1;
 		$wh = "where 1=1 "; //查询条件
 		$limit_length = 8; //设置处理意见字符串最多显示8个字符
 		$driver = new Statistic ();
 		
-		$count = $driver->get_driver_count ($company_id);
+		$count = $driver->get_driver_count ($company_id);//根据公司id查询所本公司的驾驶员总数
 		
 		if ($count > 0) {
 			$total_pages = ceil ( $count / $limit );
@@ -105,7 +62,7 @@ switch ($act) {
 		if ($start < 0)
 			$start = 0;
 		
-		$dataList = $driver->get_all_driver ( $wh, $sidx, $sord, $start, $limit,$company_id );
+		$dataList = $driver->get_all_driver ( $wh, $sidx, $sord, $start, $limit,$company_id );//得到驾驶员信息
 		
 		$response->page = $page; //分别赋值当前页,总页数，总数据条数
 		$response->total = $total_pages;
@@ -118,14 +75,14 @@ switch ($act) {
 		echo json_encode ( $response ); //打印json格式的数据
 		break;
 	
-	case "driver_time_data" :
+	case "driver_time_data" ://根据时间查询驾驶员信息
 		if (! $sidx)
 			$sidx = 1;
 		$wh = "where 1=1 "; //查询条件
 		$limit_length = 8; //设置处理意见字符串最多显示8个字符
 		$driver = new Statistic ();
 		
-		$count = $driver->sel_driver_count ($begin_data,$end_data,$company_id);
+		$count = $driver->sel_driver_count ($begin_data,$end_data,$company_id);//根据时间和公司id查询驾驶员数量
 		
 		if ($count > 0) {
 			$total_pages = ceil ( $count / $limit );
@@ -138,6 +95,7 @@ switch ($act) {
 		if ($start < 0)
 			$start = 0;
 		
+		//得到驾驶员信息
 		$dataList = $driver->get_driver_time ($begin_data,$end_data, $wh, $sidx, $sord, $start, $limit,$company_id );
 		
 		$response->page = $page; //分别赋值当前页,总页数，总数据条数
@@ -152,9 +110,9 @@ switch ($act) {
 		break;
 	
 		
-	case "vehicle_time_data" :
+	case "vehicle_time_data" ://根据选择的车辆id,公司id和起始查询和结束查询时间,查询车辆信息
 		
-		$vehicle_array = $_REQUEST ['select_vehicle']; 		
+		$vehicle_array = $_REQUEST ['select_vehicle']; 	//得到所选择车辆id	
 				
 		if (! $sidx)
 			$sidx = 1;
@@ -162,7 +120,7 @@ switch ($act) {
 		$limit_length = 8; //设置处理意见字符串最多显示8个字符
 		$driver = new Statistic ();
 			
-		$count = $driver->sel_vehicle_count ($begin_data,$end_data,$vehicle_array,$company_id);	
+		$count = $driver->sel_vehicle_count ($begin_data,$end_data,$vehicle_array,$company_id);	//在时间段和所选车辆,车辆id的驾驶员数量
 				
 		if ($count > 0) {
 			$total_pages = ceil ( $count / $limit );
@@ -175,6 +133,7 @@ switch ($act) {
 		if ($start < 0)
 			$start = 0;
 		
+		///在时间段和所选车辆,车辆id的驾驶员数据信息
 		$dataList = $driver->get_vehicle_time($vehicle_array,$begin_data,$end_data, $wh, $sidx, $sord, $start, $limit,$company_id );
 		
 		$response->page = $page; //分别赋值当前页,总页数，总数据条数
@@ -199,7 +158,7 @@ switch ($act) {
 	    break;
 
 	    
-   case  "drive_detail_data" ://显示驾驶员开车列表数据
+   case  "drive_detail_data" ://显示驾驶员详细信息列表数据
    	$driver_id=$_REQUEST["drive_id"];//得到驾驶员id 
    	if (! $sidx)
 			$sidx = 1;
@@ -207,7 +166,7 @@ switch ($act) {
 		$limit_length = 8; //设置处理意见字符串最多显示8个字符
 		$driver = new Statistic ();
 		
-		$count = $driver->drive_detail_count($driver_id);
+		$count = $driver->drive_detail_count($driver_id);//根据驾驶员id得到驾驶员详细数量
 		
 		if ($count > 0) {
 			$total_pages = ceil ( $count / $limit );
@@ -219,7 +178,7 @@ switch ($act) {
 		$start = $limit * $page - $limit;
 		if ($start < 0)
 			$start = 0;
-		
+		//根据驾驶员id得到驾驶员详细数据信息
 		$dataList = $driver->drive_detail_data($driver_id, $wh, $sidx, $sord, $start, $limit );
 		
 		$response->page = $page; //分别赋值当前页,总页数，总数据条数
@@ -313,7 +272,7 @@ switch ($act) {
 		break;
 		
 		
-	case "vstop_detail_data":
+	case "vstop_detail_data"://停车详细信息数据
 		$vehicle_id=$_REQUEST["vehicle_id"];//得到驾驶员id 
    	if (! $sidx)
 		$sidx = 1;
@@ -347,7 +306,7 @@ switch ($act) {
 		echo json_encode ( $response ); //打印json格式的数据	
 		break;
 	
-	case "driver_driving":
+	case "driver_driving"://显示开车驾驶员的姓名
 		  
 		  $param["DRIVER_DRIVING_ID"]= $_REQUEST["driver_driving_id"];
 		  $param["DRIVER_DRIVING_NAME"]= iconv("gb2312","UTF-8",$_REQUEST["driver_driving_name"]);
@@ -356,7 +315,7 @@ switch ($act) {
 		
 		  break;
 
-   case "driver_stop":
+   case "driver_stop"://显示停车驾驶员的姓名
 		  
  		  $param["DRIVER_STOP_ID"]=$_REQUEST["driver_stop_id"];
   	      $param["DRIVER_STOP_NAME"]=iconv("gb2312","UTF-8",$_REQUEST["driver_stop_name"]);;
@@ -364,8 +323,9 @@ switch ($act) {
 		  echo $GLOBALS ['db']->display ($param, $act );
 		
   	      break;
-
-   case "vehicle_driving":
+    
+   
+   case "vehicle_driving"://显示启用车辆的车牌号码
    	      
 	   	   $param["VE_DRIVING_ID"]=$_REQUEST["vehicle_drinving_number"];
 	   	   $param["VE_DRIVING_NUMBER_PLATE"]=iconv("gb2312","UTF-8",$_REQUEST["vehicle_drinving_numberplate"]);;
@@ -374,7 +334,8 @@ switch ($act) {
 		   
    	       break;
 	
-   case "vehicle_stop":
+   
+   case "vehicle_stop"://显示停用车辆的车牌号码
    	 	   
    	       $param["VE_STOP_ID"]=$_REQUEST["vehicle_stop_number"];
    	 	   $param["VE_STOP_NUMBER_PLATE"]=iconv("gb2312","UTF-8",$_REQUEST["vehicle_stop_numberplate"]);
