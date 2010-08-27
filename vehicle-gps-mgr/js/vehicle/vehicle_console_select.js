d4c8f061d664e86566e777c8d675a50f6b0c6cda
zhaojiangwei6464@163.com <zhaojiangwei6464@163.com@2fe7dcaa-6ef8-bcf5-8241-07c18bce28a8>
2010-08-27 09:45:07 +0000

    添加抽取出来 的选择车辆JS

---
+++ vehicle-gps-mgr/js/vehicle/vehicle_console_select.js
@@ -0,0 +1,34 @@
+$("#tabs").tabs(); //加载JQUERY UI
+			
+
+			//选择本组所有车辆
+	        $(".selectall").click(function(){
+				if($(this).attr("checked")==true){
+				    $("input[name='"+$(this).attr("value")+"']").each(function(){
+					    $(this).attr("checked",true);
+					});
+				}else{
+				      $("input[name='"+$(this).attr("value")+"']").each(function(){
+					    $(this).attr("checked",false);
+				});
+				}
+			});
+
+	        //选择所有车辆
+			$("#all").click(function(){
+			     if($(this).attr("checked")==true){
+				     $(".vehicle").each(function(){
+					     $(this).attr("checked",true);
+					 });
+				     $(".selectall").each(function(){
+					     $(this).attr("checked",true);
+					 });
+				 }else{
+				     $(".vehicle").each(function(){
+					     $(this).attr("checked",false);
+					 });
+				     $(".selectall").each(function(){
+					     $(this).attr("checked",false);
+					 });
+				 }
+			});
