2348f66332bc4033e1f3db6e47b07c5f160b4a23
zhaojiangwei6464@163.com <zhaojiangwei6464@163.com@2fe7dcaa-6ef8-bcf5-8241-07c18bce28a8>
2010-09-01 02:05:35 +0000

    添加年检提示js

---
+++ vehicle-gps-mgr/js/home_page/as_date.js
@@ -0,0 +1,25 @@
+$(".modify_as").click(function(){
+	var id = $(this).attr("id")/3;
+	$("#tijiao"+id).show();
+});
+
+$(".commit_new_date").click(function(){
+	var id = $(this).attr("name");
+	var date_id = $(this).attr("id")*2;
+	var real_date = $("#"+date_id).val();
+	$.get("index.php?a=104&vehicle_id="+id+"&new_date="+real_date,function(data){
+		alert(data);
+	});
+	$("#tijiao"+id).hide();
+});
+
+$(".new_as_date").datetimepicker({
+	 ampm: false,//上午下午是否显示  
+	 timeFormat: 'hh:mm:ss',//时间模式  
+	 stepHour: 1,//拖动时间时的间隔  
+	 dateFormat:"yy/mm/dd", //日期格式设定  
+	 showHour: true,//是否显示小时，默认是true  
+	 showMinute:false,
+	 showSecond:false,
+	 createButton:false
+ });	
