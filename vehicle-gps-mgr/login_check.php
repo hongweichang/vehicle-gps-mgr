fcdf874326f1f6332985eee7e8e49a90883529bc
starecho@gmail.com <starecho@gmail.com@2fe7dcaa-6ef8-bcf5-8241-07c18bce28a8>
2010-11-26 02:13:04 +0000

    删除多余的注释代码。

--- vehicle-gps-mgr/login_check.php
+++ vehicle-gps-mgr/login_check.php
@@ -12,12 +12,8 @@
 	$user = new User();
 	if(!$user->login($user_name,$user_pass,$companyloginid))
 	{
-		//$errormsg = $user->message;//"用户名或密码错误，登录失败！";
 		echo(-1);
-		//die();
 	}
-	//goto_url(URL('user',"user.php","login_success"));
-	//Header("Location: index.php?a=1003");
 	if($companyloginid=="system"){
 		echo(2);
 	}else{
